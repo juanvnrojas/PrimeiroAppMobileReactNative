a6042b2b50d388cf642f588a1cb9bb81e5406a51
Juan Vila Nova Rojas Moreno <juanvnrojas@gmail.com>
2026-02-11 10:14:47 -0300

    Enhance App with login and registration features
    Added email and password input fields with buttons for login and registration.

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image, Alert } from 'react-native';
 
 export default function App() {
+  const [email, botarEmail] = React.useState('');
+  const [senha, botarSenha] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Juan (Paulista) e João Paz - 3B</Text>
+      <Image
+        style={styles.img}
+        source={{
+          uri: 'https://0.academia-photos.com/30234330/13546231/115364738/s200_pablo.sp_ndola.jpeg',
+        }}
+      />
+
+      <Text>Email</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={botarEmail}
+      />
+
+      <Text>Senha</Text>
+      <TextInput
+        style={styles.input}
+        value={senha}
+        onChangeText={botarSenha}
+      />
+
+      <Button
+        title="Logar"
+        onPress={() => Alert.alert('Email: ' + email)}
+      />
+
+      <Button
+        title="Cadastrar-se"
+        onPress={() => Alert.alert('Cadastro')}
+      />
+
+      <Text>Esqueceu a senha?</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +52,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#014995',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  img: {
+    width: 200,
+    height: 200
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 250,
+    backgroundColor: '#fff'
   },
 });
